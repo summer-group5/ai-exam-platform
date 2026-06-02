a70dd77aa098ae37fe579657fe7b902866acec21
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-06-02 14:09:36 +0300

    students page

--- frontend/src/pages/Studentspage.jsx
+++ frontend/src/pages/Studentspage.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import Topnav from '../components/topnav/Topnav'
-
-
+import Toolcard from '../components/toolcard/toolcard'
+import './Studentspage.css'
 export default function Studentspage() {
+  
+  const tools = [
+    { id: 1, title: "Java basics", path: "/" },
+    { id: 2, title: "Linux basics", path: "/" },
+    { id: 3, title: "HTML5 and css", path: "/" },
+    { id: 4, title: "Advanced mobile development", path: "/" },
+    { id: 5, title: "Web development project", path: "/" },
+    { id: 6, title: "Svenska för arbetlivet", path: "/" }
+  
+  
+  ] 
+
   return (
   <>
   <Topnav/>
-    
-    
-    <div>studentspage
-
-
+  
+    <div className='students-page'>
+ <h1>My courses</h1>
+ <div className='tools-grid'>
+    {tools.map(tool => (
+    <Toolcard
+    key={tool.id}
+    title={tool.title}
+    path={tool.path}
+    /> 
+  
+  ))}
+ 
+ </div>
+ 
     </div>
    </>
   )
