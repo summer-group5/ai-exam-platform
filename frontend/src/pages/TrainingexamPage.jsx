551a5973fce3555f51812394fdb6da65161acac7
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-06-05 14:04:36 +0300

    crated branch and placeholder for training exam

---
+++ frontend/src/pages/TrainingexamPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TrainingexamPage() {
+  return (
+    
+    <>
+    
+    <div className='training-exam-container'>
+        TrainingexamPage
+    </div>
+    
+    </>
+  )
+}
+
