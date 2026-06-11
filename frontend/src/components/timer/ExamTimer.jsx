02cc1855264d26d9647bd95032776c6bfd6e1603
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-06-11 15:08:14 +0300

    timer component started

---
+++ frontend/src/components/timer/ExamTimer.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react'
+
+
+export default function ExamTimer() {
+  
+    initialHours = 1,
+  initialMinutes = 30,
+  initialSeconds = 0,
+  onFinish
+
+    
+    return (
+    <div className='exam-timer'>
+        
+        ExamTimer
+        
+        </div>
+  )
+}
