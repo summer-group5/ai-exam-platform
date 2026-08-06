3c04b80bf9b30e476e49572f360108a10ef2ed12
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-08-06 12:56:45 +0300

    back end connection to exam monitoring page and logs tested

--- frontend/src/pages/ExamMonitoringpage.jsx
+++ frontend/src/pages/ExamMonitoringpage.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './ExamMonitoringpage.css'
+
+import {
+  getExamSessions,
+  getAllMonitoringEvents
+} from '../services/monitoringService';
+
 
 export default function ExamMonitoringpage() {
   
-     const students = [
-        {
-            id: 1,
-            name: "Alice",
-            status: "Active",
-            question: 12,
-            progress: "12 / 20",
-            warnings: 0
-        },
-        {
-            id: 2,
-            name: "Bob",
-            status: "Tab Changed",
-            question: 10,
-            progress: "10 / 20",
-            warnings: 2
-        },
-        {
-            id: 3,
-            name: "Charlie",
-            status: "Submitted",
-            question: 20,
-            progress: "20 / 20",
-            warnings: 0
-        }
-    ];
+    const [sessions, setSessions] = useState([]);
+    const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const sessionData = await getExamSessions();
+        const eventData = await getAllMonitoringEvents();
+
+        setSessions(sessionData);
+        setEvents(eventData);
+
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    loadData();
+  }, []);
+
+    console.log(sessions);
+    console.log(events);
   
   
-  
+
+
+
   return (
     <>
     <div className="ExamMonitoring-page">
@@ -41,29 +45,7 @@ export default function ExamMonitoringpage() {
         </div>
        
        <div className="ExamMonitoring-container">
-  <table>
-    <thead>
-      <tr>
-        <th>Student</th>
-        <th>Status</th>
-        <th>Current Question</th>
-        <th>Progress</th>
-        <th>Warnings</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      {students.map(student => (
-        <tr key={student.id}>
-          <td>{student.name}</td>
-          <td>{student.status}</td>
-          <td>{student.question}</td>
-          <td>{student.progress}</td>
-          <td>{student.warnings}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
+  
 </div>
         
         
